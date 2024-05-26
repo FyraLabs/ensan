fd5879fdaf11ecf7a6ff632d0a871ce420bba55f
Cappy Ishihara <cappy@cappuchino.xyz>
2024-05-27 01:57:07 +0700

    Add Clippy hints, Better documentation, better test examples.
    Co-authored-by: madomado <madonuko@outlook.com>

--- README.md
+++ README.md
@@ -4,8 +4,19 @@
 ![Crates.io License](https://img.shields.io/crates/l/ensan)
 
 
-> A corrosive language parser for a corroded language.
+> A corrosive expression engine for a corroded language.
+
+Extended (Dynamic) Evaluation Engine for `hcl-rs`.
+
+This crate aims to provide an extended evaluation engine for `hcl-rs`.
+It simplifies the usage of `hcl-rs` by re-implementing features in `hcldec` library.
+It includes advanced DAG graph building and evaluation features, allowing evaluation of references in the current document without needing additional variables in the context, similar to Terraform's behavior.
+
+## Features
+ - Re-implementations of the HCL2 built-in functions from Terraform, Packer, and other HashiCorp tools.
+ - Out-of-the-box support for evaluating references in the current document.
+ - Simple API for evaluating entire documents with serde serialization support.
+
+For usage, see the documentation for the [`engine`] module.
 
 
-
-Extended Evaluation Engine for `hcl-rs`.
--- src/engine.rs
+++ src/engine.rs
@@ -13,12 +13,12 @@
 //! obj = {
 //!     key = "value"
 //! }
-//! my_block "hai" {
+//! my_block "hai" "bai" {
 //!     foo = "bar"
 //!     baz = "nya"
 //! }
 //!
-//! nested_str = "${my_block.hai.foo}"
+//! nested_str = "${my_block.hai.bai.foo}"
 //! "#;
 //!
 //! let expected = r#"
@@ -27,7 +27,7 @@
 //! obj = {
 //!     key = "value"
 //! }
-//! my_block "hai" {
+//! my_block "hai" "bai" {
 //!     foo = "bar"
 //!     baz = "nya"
 //! }
@@ -46,18 +46,17 @@
 //! assert_eq!(body, body2);
 //! ```
 
+use core::borrow::BorrowMut;
 use hcl::{
     eval::{Context, Evaluate},
     Value,
 };
-use std::borrow::BorrowMut;
-// #[hcl_func(ParamType::String)]
-// fn yamldecode(args: FuncArgs) -> Result<Value, String>
 
-// YAML de(ser)ialization functins
+/// Internal result type
 type Res<T> = Result<T, crate::Error>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum VarScope {
     Var(String, Value),
     Scope(String, VarScopes),
@@ -65,6 +64,7 @@ pub enum VarScope {
 
 impl VarScope {
     #[must_use]
+    #[inline]
     pub fn get_scope_mut(&mut self, scope: &str) -> Option<&mut VarScopes> {
         match self {
             Self::Scope(key, varscopes) if key == scope => Some(varscopes),
@@ -72,6 +72,7 @@ impl VarScope {
         }
     }
     #[must_use]
+    #[inline]
     pub fn get_scope_ref(&self, scope: &str) -> Option<&VarScopes> {
         match self {
             Self::Scope(key, varscopes) if key == scope => Some(varscopes),
@@ -84,6 +85,7 @@ impl VarScope {
 pub struct VarScopes(Vec<VarScope>);
 
 impl From<Vec<VarScope>> for VarScopes {
+    #[inline]
     fn from(value: Vec<VarScope>) -> Self {
         Self(value)
     }
@@ -91,14 +93,17 @@ impl From<Vec<VarScope>> for VarScopes {
 
 impl VarScopes {
     #[must_use]
+    #[inline]
     pub fn as_vec_mut(&mut self) -> Vec<&mut VarScope> {
         self.0.iter_mut().collect()
     }
     #[must_use]
+    #[inline]
     pub fn as_vec_ref(&self) -> Vec<&VarScope> {
         self.0.iter().collect()
     }
     #[must_use]
+    #[inline]
     pub fn list_in_scope_mut(&mut self, scope: &[&str]) -> Vec<&mut VarScope> {
         let [first, remaining @ ..] = scope else {
             return self.as_vec_mut();
@@ -148,7 +153,7 @@ impl VarScopes {
         if let Some(s) = self.0.iter_mut().find_map(|s| s.get_scope_mut(first)) {
             s.set(rest, key, value);
         } else {
-            let mut new = VarScopes::default();
+            let mut new = Self::default();
             new.set(rest, key, value); // TODO: optimizations
             self.0.push(VarScope::Scope(first.to_string(), new));
         }
@@ -157,23 +162,27 @@ impl VarScopes {
 
 /// Engine for parsing hcl strings
 #[derive(Debug, Clone)]
+#[non_exhaustive]
 pub struct Engine<S: AsRef<str>> {
+    /// string hcl
     pub content: S,
-    scope: Vec<String>,
+    /// (variable) scope during parsing
+    pub scope: Vec<String>,
+    /// variable list
     pub varlist: VarScopes,
 }
 
 impl<S: AsRef<str>> From<S> for Engine<S> {
     fn from(value: S) -> Self {
-        Engine {
+        Self {
             content: value,
             scope: vec![],
-            varlist: Default::default(),
+            varlist: VarScopes::default(),
         }
     }
 }
 impl<S: AsRef<str>> Engine<S> {
-    fn init_ctx(&self, ctx: &mut Context) {
+    fn init_ctx(ctx: &mut Context) {
         use crate::functions;
         crate::add_hcl_fns!(ctx =>
             functions::yamlencode[String],
@@ -202,7 +211,7 @@ impl<S: AsRef<str>> Engine<S> {
 
     fn parse_struct(&mut self, structure: &mut hcl::Structure) -> Res<()> {
         let mut ctx = self.varlist.to_hcl_ctx(&self.scope);
-        self.init_ctx(&mut ctx);
+        Self::init_ctx(&mut ctx);
         if let Some(attr) = structure.as_attribute_mut() {
             let val = attr.expr.evaluate(&ctx)?;
             self.varlist
