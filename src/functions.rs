8d3d61d9ce857e75663b07f8605af78ae121a313
Cappy Ishihara <cappy@cappuchino.xyz>
2024-05-26 23:38:06 +0700

    add more functions

--- src/functions.rs
+++ src/functions.rs
@@ -1,15 +1,16 @@
-use hcl::{eval::{Context, Evaluate, FuncArgs, FuncDef, ParamType}, Value};
-
+use hcl::{
+    eval::{Context, Evaluate, FuncArgs, FuncDef, ParamType},
+    Value,
+};
 
 type FnRes = Result<Value, String>;
 
-
 /// Serializes YAML from a string to HCL
-/// 
+///
 /// Accepts: String
-/// 
+///
 /// Returns: Value (Any)
-/// 
+///
 /// Example: yamldecode("key: value") => { key = "value" }
 fn yamldecode(args: FuncArgs) -> Result<Value, String> {
     let args = args.iter().next().ok_or("No arguments provided")?;
@@ -18,25 +19,27 @@ fn yamldecode(args: FuncArgs) -> Result<Value, String> {
 }
 
 /// Deserializes HCL from a string to YAML
-/// 
+///
 /// Accepts: String
-/// 
+///
 /// Returns: String
-/// 
+///
 /// Example: yamlencode({ key = "value" }) => "key: value"
 fn yamlencode(args: FuncArgs) -> Result<Value, String> {
     let args = args.iter().next().ok_or("No arguments provided")?;
 
-    Ok(Value::String(serde_yml::to_string(&args.to_string())
-    .map_err(|e| format!("Failed to serialize YAML: {}", e))?))
+    Ok(Value::String(
+        serde_yml::to_string(&args.to_string())
+            .map_err(|e| format!("Failed to serialize YAML: {}", e))?,
+    ))
 }
 
 /// Get value from environment variable
-/// 
+///
 /// Accepts: String
-/// 
+///
 /// Returns: String
-/// 
+///
 /// Example: env("HOME") => "/home/user"
 fn env(args: FuncArgs) -> Result<Value, String> {
     let args = args.iter().next().ok_or("No arguments provided")?;
@@ -46,13 +49,12 @@ fn env(args: FuncArgs) -> Result<Value, String> {
         .map_err(|e| format!("Failed to get environment variable: {}", e))?)
 }
 
-
 /// Make all characters in a string lowercase
-/// 
+///
 /// Accepts: String
-/// 
+///
 /// Returns: String
-/// 
+///
 /// Example: lower("HELLO") => "hello"
 fn lower(args: FuncArgs) -> FnRes {
     let args = args.iter().next().ok_or("No arguments provided")?;
@@ -60,24 +62,23 @@ fn lower(args: FuncArgs) -> FnRes {
 }
 
 /// Make all characters in a string uppercase
-/// 
+///
 /// Accepts: String
-/// 
+///
 /// Returns: String
-/// 
+///
 /// Example: upper("hello") => "HELLO"
 fn upper(args: FuncArgs) -> FnRes {
     let args = args.iter().next().ok_or("No arguments provided")?;
     Ok(Value::String(args.to_string().to_uppercase()))
 }
 
-
 /// Split a string into a list of string with a separator
-/// 
+///
 /// Accepts: String, String
-/// 
+///
 /// Returns: [String]
-/// 
+///
 /// Example: split(",", "a,b,c") => ["a", "b", "c"]
 fn split(args: FuncArgs) -> FnRes {
     let mut args = args.iter();
@@ -94,7 +95,8 @@ fn split(args: FuncArgs) -> FnRes {
 
     let args = args.to_string();
 
-    let splitted: Vec<Value> = args.split(&sep)
+    let splitted: Vec<Value> = args
+        .split(&sep)
         .map(|s| Value::String(s.to_string()))
         .collect();
 
@@ -102,11 +104,11 @@ fn split(args: FuncArgs) -> FnRes {
 }
 
 /// Join a list of strings into a single string with a separator
-/// 
+///
 /// Accepts: String, [String]
-/// 
+///
 /// Returns: String
-/// 
+///
 /// Example: join(",", ["a", "b", "c"]) => "a,b,c"
 fn join(args: FuncArgs) -> FnRes {
     let mut args = args.iter();
@@ -116,20 +118,27 @@ fn join(args: FuncArgs) -> FnRes {
     }
     let sep = args.next().ok_or("No separator provided")?;
 
-
     let sep = sep.to_string();
 
     // Second argument is the string to split
     let args = args.next().ok_or("No arguments provided")?;
 
-
     let args = args.as_array().unwrap_or_else(|| unreachable!());
 
-    let args: Vec<String> = args.iter()
-        .map(|s| s.to_string())
-        .collect();
+    let args: Vec<String> = args.iter().map(|s| s.to_string()).collect();
 
     Ok(Value::String(args.join(&sep)))
+}
 
-    
+/// Get the length of a string
+///
+/// Accepts: String
+///
+/// Returns: Number
+///
+/// Example: strlen("hello") => 5
+fn strlen(args: FuncArgs) -> FnRes {
+    let args = args.iter().next().ok_or("No arguments provided")?;
+    let len = args.to_string().len();
+    Ok(len.into())
 }
